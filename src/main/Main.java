ee5dd6f91adc27f3216e47b39cbbacdae6355ba8
Rafael Sousa <akllwzk@gmail.com>
2015-06-14 15:37:53 -0300

    Adiciona modelo pessoa

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,9 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("HelloWorld!!");
+		Pessoa p = new Pessoa();
+		p.setNome("John");
+		System.out.println("Hello " + p.getNome() + "!!!");
 	}
 
 }

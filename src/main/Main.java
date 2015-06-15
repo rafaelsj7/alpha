e7ae7fee2242d4e9f4fc0e5d9b78cfd40375844a
Rafael Sousa <akllwzk@gmail.com>
2015-06-14 22:28:56 -0300

    Atualizar saida de mensagem

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,9 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		Pessoa p = new Pessoa();
 		p.setNome("John");
-		System.out.println("Hello " + p.getNome() + "!!!");
 		Animal cachorro = new Animal();
 		p.setPet(cachorro);
+		System.out.println("Hello " + p.getNome() + "!!!");
 	}
 
 }
